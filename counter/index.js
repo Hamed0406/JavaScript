f48f92186af3ae1d8234c3a7c7400b529b9fcb3f
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-07-20 13:17:17 +0200

    don counter

--- counter/index.js
+++ counter/index.js
@@ -6,6 +6,9 @@ let connt=0
 let countEl=document.getElementById("count-el")
 console.log(connt)
 console.log(countEl)
+let messageEl=document.getElementById("message-save")
+let pervoiusEntry=document.getElementById("pervious-entery")
+let countTotal=0
 
 
 function incerment()
@@ -22,5 +25,16 @@ function incerment()
 
 function save()
 {
-    console.log(connt)
+    printEntry()
+    countTotal+=connt
+     messageEl.innerText="People had been enterd "+countTotal
+     connt=0
+     countEl.textContent=0
+    console.log(messageEl)
+}
+
+function printEntry()
+{
+    let counterStr = connt + " - "
+    pervoiusEntry.textContent+=counterStr
 }
