e95f1ba271a2eac58e311be21331234e7851bcc2
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-07-21 11:39:44 +0200

    fix script link

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,32 +1,36 @@
-let firtCard = 6;
-let secondCard = 11;
+let firtCard = getRndInteger();
+let secondCard = getRndInteger();
 let hasBlackJack = false;
 let isAlive = true;
 
-let sum = firtCard + secondCard+6;
+let sum = firtCard + secondCard;
 
-let message=""
+let messageEl=document.getElementById("message-el")
 
-let messag1 = "Do you want to draw a new card ?";
-let messag2 = "Whoo!! you have BLACKJACK!!";
-let message3 = "You are out of the game";
-
+console.log(messageEl)
+let message= ""
 
 
 
 
-function startGmae(){
+
+function startGame(){
+
     if (sum <= 20) {
-        message=messag1
+        message= "Do you want to draw a new card ?"
       isAlive = true;
     } else if (sum === 21) {
-        message=messag2
+       message= "Whoo!! you have BLACKJACK!!"
       hasBlackJack = true;
     } else {
-    message=message3
+    message= "You are out of the game";
         isAlive=false
     }
     
-    console.log(message)
+   messageEl.textContent=message
 
 }
+
+function getRndInteger() {
+    return Math.floor(Math.random() * (11 - 1 + 1) ) + 1;
+  }
