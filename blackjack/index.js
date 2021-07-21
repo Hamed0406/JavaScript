b4380dbaa34dd2a27e64536052ab80a90a61a65e
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-07-21 11:16:48 +0200

    Add button to table

--- blackjack/index.js
+++ blackjack/index.js
@@ -1,6 +1,32 @@
-let firtCard=6
-let secondCard=9
+let firtCard = 6;
+let secondCard = 11;
+let hasBlackJack = false;
+let isAlive = true;
 
-let sum=firtCard+secondCard
+let sum = firtCard + secondCard+6;
 
-console.log(sum)
+let message=""
+
+let messag1 = "Do you want to draw a new card ?";
+let messag2 = "Whoo!! you have BLACKJACK!!";
+let message3 = "You are out of the game";
+
+
+
+
+
+function startGmae(){
+    if (sum <= 20) {
+        message=messag1
+      isAlive = true;
+    } else if (sum === 21) {
+        message=messag2
+      hasBlackJack = true;
+    } else {
+    message=message3
+        isAlive=false
+    }
+    
+    console.log(message)
+
+}
